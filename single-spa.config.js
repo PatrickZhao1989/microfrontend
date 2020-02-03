d602c819237ebee971d4b65153a7023819c4256e
Patrick Zhao <PatrickZhao@ssw.com.au>
2020-02-03 11:32:11 +1100

    inital commit: single-spa set up; angularJS app; shell-routing setup; angular 8 app init

---
+++ single-spa.config.js
@@ -0,0 +1,39 @@
+import { registerApplication, start } from 'single-spa'
+
+registerApplication(
+	// Name of single-spa application, corresponds to the id tag in index.html
+	'home',
+	// Our loading function
+	() => import('./src/home/home.app.js'),
+	// Our activity function
+	() => location.pathname === "" ||
+		location.pathname === "/" ||
+		location.pathname.startsWith('/home')
+);
+registerApplication(
+	// Name of single-spa application, corresponds to the id tag in index.html
+	'navBar',
+	// Loading function
+	() => import('./src/navBar/navBar.app.js').then(module => module.navBar),
+	// Activity function
+	() => true
+);
+registerApplication(
+	// Name of single-spa application, corresponds to the id tag in index.html
+	'angularJS',
+	// L
+	() => import('./src/angularJS/angularJS.app.js'),
+	//
+	() => pathPrefix('angularJS')
+);
+start();
+
+
+
+
+
+function pathPrefix(prefix) {
+	return function (location) {
+		return location.pathname.startsWith(prefix);
+	}
+}
